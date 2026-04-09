b9ff4c9b92b94db405c271014435851e69469b2d
Marko Jeremic <markojeremic03@gmail.com>
2026-04-09 02:10:33 +0200

    Dodane zavisnosti

--- server-spring/src/main/java/server/App.java
+++ server-spring/src/main/java/server/App.java
@@ -1,5 +1,31 @@
 package server;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
+    public static void main(String[] args) {
+
+        SpringApplication app = new SpringApplication(App.class);
+        
+
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        
+        Map<String, Object> envProps = new HashMap<>();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue()); 
+            envProps.put(entry.getKey(), entry.getValue());
+        });
+        
+        app.setDefaultProperties(envProps);
+        app.run(args);
+
+    }
 
 }
